c1f3deaa34464efbf79cbde546e705f16701424a
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-07-19 15:54:10 +0530

    styling and given responsive behaviour

--- app/newComponents/TodoList.tsx
+++ app/newComponents/TodoList.tsx
@@ -20,36 +20,33 @@ const TodoList = () => {
         filterTodos = filterTodos.filter((todo) => todo.completed)
     }
 
-    return <ul className={""}>
-
-
+    return <ul className={"divide-y divide-blue-500"}>
         {filterTodos.map((todo) => {
-            return <li key={todo.id} className={"main-task"}>
-
-
-                    <input className={"cursor-pointer"}
+            return <li key={todo.id} className={"main-task flex items-start justify-between py-4"}>
+                <div className={"flex gap-5 items-start"}>
+                    <input className={"cursor-pointer h-4 w-4 mt-1"}
                            type="checkbox"
                            name={""}
                            id={`todo-${todo.id}`}
                            checked={todo.completed}
                            onChange={() => todoAsCompleted(todo.id)}
+                           autoComplete={false}
                     />
 
                     <label htmlFor={`todo-${todo.id}`}
                            className={"font-bold text-base text-blue-500"}>
                         {todo.task}
                     </label>
+                </div>
 
 
                 {
                     todo.completed && (
 
-                            <button className={"text-red-500"}
-                                    type={"button"} onClick={() => handleDeleteTodo(todo.id)}>
-                                <AiFillDelete size={20}/>
-                            </button>
-
-
+                        <button className={"text-red-500"}
+                                type={"button"} onClick={() => handleDeleteTodo(todo.id)}>
+                            <AiFillDelete size={20}/>
+                        </button>
                     )
                 }
             </li>
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,15 @@ import FilterNavbar from "./newComponents/FilterNavbar";
 
 
 const Page = () => {
-    return <main className={"mt-5 px-5"}>
-        <h2 className={"text-center text-blue-600 font-semibold text-2xl mb-6"}>
-            Task Manager
-        </h2>
-        <FilterNavbar/>
-        <AddTodo/>
-        <TodoList/>
+    return <main className={"container mt-5 px-5 flex flex-col justify-center mx-auto max-w-2xl"}>
+
+            <h2 className={"text-center text-blue-600 font-semibold text-2xl mb-6"}>
+                Task Manager
+            </h2>
+            <FilterNavbar/>
+            <AddTodo/>
+            <TodoList/>
+
     </main>
 }
 
